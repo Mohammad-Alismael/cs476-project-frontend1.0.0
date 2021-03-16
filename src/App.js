16c23fde601b1f95f2219654e8167c6a7c1e9881
mhd <mhdhd@gmail.com>
2021-03-16 11:14:38 +0400

    fixing routes issue

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route,Redirect } from 'react-router-dom';
+import {BrowserRouter as Router, Route,Redirect,Switch } from 'react-router-dom';
 import LandingPage from "./Pages/LandingPage";
 import LoginForm from "./Pages/LoginForm";
 import RegisterForm from "./Pages/RegisterForm";
 import ProfileSettings from "./Pages/ProfileSettings";
+
 function App() {
   return (
       <Router>
-        {/* <Route path="/">
-          <Redirect to="/login"/>
-        </Route> */}
-        {/*<Route  path="/login" render= {props =>(*/}
-        {/*    <>*/}
-        {/*      <LoginForm/>*/}
-        {/*    </>*/}
-
-        {/*)}/>*/}
-        <Route path="/login" component={LoginForm} />
-        <Route path="/register" component={RegisterForm} />
-        <Route path="/" component={LandingPage} />
-        <Route path="/profile-settings" component={ProfileSettings} />
-        <Route path="/log-out">
-          <Redirect to="/login" />
-        </Route>
+          <div>
+              <Switch>
+                  <Route exact path="/" component={LandingPage}/>
+                  <Route exact path="/login" component={LoginForm}/>
+                  <Route exact path="/register" component={RegisterForm} />
+                  <Route exact path="/profile-settings" component={ProfileSettings}/>
+                  <Route exact path="/log-out">
+                      <Redirect to="/login"/>
+                  </Route>
+              </Switch>
+          </div>
       </Router>
 
 
