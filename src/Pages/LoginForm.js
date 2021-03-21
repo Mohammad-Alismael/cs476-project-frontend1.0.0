d1d9e0d0a171509287351d974283c9df13ec11a0
mhd <mhdhd@gmail.com>
2021-03-21 14:47:48 +0400

    convert it to react

--- src/Pages/LoginForm.js
+++ src/Pages/LoginForm.js
@@ -1,17 +1,31 @@
-import React, {Component} from 'react';
-import {Card,CardBody} from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/LandingPage.css'
+import {Component,Link} from 'react';
+import '../Pages/css/LoginPage.css'
+import img from '../Pages/css/Design.png'
 class LoginForm extends Component {
+
     render() {
         return (
-            <div>
-                <Card className="myCard">
-                    <CardBody>
-
-                    </CardBody>
-                </Card>
+            <div className="middle">
+                 <div className="middle-left">
+                     <img className="middle-img" src={img} alt="" />
+                 </div>
+            <div className="middle-right">
+                <h1>Login</h1>
+                <p>Welcome back, please login to your account.</p>
+                <form className="user-info">
+                    <label className="email" htmlFor="email">Email</label>
+                    <input type="email" className="form-input" name="email" required/><br/>
+                    <label className="password" htmlFor="password">Password</label><br/>
+                 <input type="password" className="form-input" name="password" required/><br/>
+                <input type="checkbox" className="checkbox-form" name="terms" value=""/>
+                 <label htmlFor="terms"> Remember me</label>
+                {/*<Link className="forgot-password" to="">Forgot Password?</Link><br/>*/}
+               <button className="login-butt" type="button" name="button">Login</button>
+                <button className="register-butt" type="button" name="button">Register</button>
+                    </form>
+                </div>
             </div>
+
         );
     }
 }
