63274ca4a25fea302b838e8382e56afb49a96d11
mhd <mhdhd@gmail.com>
2021-03-22 22:51:01 +0400

    fixing issues

--- src/Pages/RegisterForm.js
+++ src/Pages/RegisterForm.js
@@ -7,29 +7,30 @@ class RegisterForm extends Component {
 
     render() {
         return (
-           <Container fluid>
-            <Row>
-              <div class="col-lg-6 col-md-6 col-sm-6" >
-                <img src={img} alt="" />
-              </div>
-              <div class="col-lg-4 col-md-4 col-sm-4" style={{background: 'white'}}>
-                <h1>Create Account</h1>
-                <p>Fill the below form to create a new account.</p>
-                  <form class="user-info">
-                    <input type="text" class="form-input" name="fname" placeholder="Username" required/><br/>
-                    <label for="fname"></label><br/>
-                    <input type="email" class="form-input" name="email" placeholder="Email" required/><br/>
-                    <label for="password"></label><br/>
-                    <input type="password" class="form-input" name="password" placeholder="Password" required/><br/>
-                    <label for="confirm-password"></label><br/>
-                    <input type="password" class="form-input confirm-password" name="confirm-password" placeholder="Confirm Password" required/><br/>
-                    <input type="checkbox" class="checkbox-form" name="terms" value="t"/>
-                    <label for="terms"> I accept the terms & conditions.</label><br/>
-                    <button className="login-button" type="button" name="button" style={{marginTop: '10px'}}>Register</button>
-                  </form>
-              </div>
-            </Row>
-            </Container>
+            <div className="middle">
+                <div className="middle-left">
+                    <img className="middle-img" src={img} alt="" />
+                </div>
+                <div className="middle-right">
+                    <h1>Create Account</h1>
+                    <p>Fill the below form to create a new account.</p>
+                    <form className="user-info">
+                        <input type="text" className="form-input" name="fname" placeholder="Name" required/><br/>
+                            <label htmlFor="fname"></label><br/>
+                            <input type="email" className="form-input" name="email" placeholder="Email" required/><br/>
+                                <label htmlFor="password"></label><br/>
+                                <input type="password" className="form-input" name="password" placeholder="Password"
+                                       required/><br/>
+                                    <label htmlFor="confirm-password"></label><br/>
+                                    <input type="password" className="form-input" name="confirm-password"
+                                           placeholder="Confirm Password" required/><br/>
+                                        <input type="checkbox" className="checkbox-form" name="terms" value="t"/>
+                                            <label htmlFor="terms"> I accept the terms & conditions.</label><br/>
+                                            <button className="login-button" type="button" name="button">Login</button>
+                                            <button className="register-button" type="button" name="button">Register</button>
+                    </form>
+                </div>
+            </div>
     );
     }
 }
