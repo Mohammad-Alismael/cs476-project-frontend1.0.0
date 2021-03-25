920dcd117bcc01b5b22f3ef39c914b73e7b852c8
mhd <mhdhd@gmail.com>
2021-03-25 10:56:08 +0400

    category page 100% done

--- src/Components/FilterBar.js
+++ src/Components/FilterBar.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Card, CardBody, FormGroup, Input, Label} from "reactstrap";
+import {Button, Card, CardBody, Col, FormGroup, Input, Label, Row} from "reactstrap";
 import '../Pages/css/FilterBar.css'
 class FilterBar extends Component {
     render() {
@@ -27,10 +27,78 @@ class FilterBar extends Component {
                         <hr/>
                         <h6 className="filter-heading d-none d-lg-block">Price</h6>
                         <FormGroup>
-                            From <Input type="range"/>
-                            To <Input type="range" />
+                            <Row>
+                                <Col xl={4}>
+                                    From <Input type="number" min={0}/>
+                                </Col>
+                                <Col xl={4}>
+                                    To <Input type="number" />
+                                </Col>
+                                <Col xl={4}>
+                                    <Button id={'Go-btn'}>GO</Button>
+                                </Col>
+                            </Row>
                         </FormGroup>
                         <hr/>
+                        <FormGroup>
+                            <Label>Sort By</Label>
+                            <Input type="select">
+                                <option>Recommend</option>
+                                <option>Low to High</option>
+                                <option>High to Low</option>
+                            </Input>
+                        </FormGroup>
+                        <hr/>
+                        <FormGroup>
+                            <Label>Display</Label>
+                            <Input type="select">
+                                <option>50 per page</option>
+                                <option>100 per page</option>
+                                <option>150per page</option>
+                            </Input>
+                        </FormGroup>
+                        <hr/>
+                        <h6 className="filter-heading d-none d-lg-block">Ratings</h6>
+                        <Col>
+                            <Row onClick={()=>(alert("4 stars"))}>
+                                <i className="material-icons ratingStars">star</i>
+                                <i className="material-icons ratingStars">star</i>
+                                <i className="material-icons ratingStars">star</i>
+                                <i className="material-icons ratingStars">star</i>
+                                <i className="material-icons ratingStars">star_border</i>
+                                <span>& up</span>
+                                <span className={'HowMany'}>(43)</span>
+                            </Row>
+                            <Row onClick={()=>(alert("3 stars"))}>
+                                <i className="material-icons ratingStars">star</i>
+                                <i className="material-icons ratingStars">star</i>
+                                <i className="material-icons ratingStars">star</i>
+                                <i className="material-icons ratingStars">star_border</i>
+                                <i className="material-icons ratingStars">star_border</i>
+                                <span>& up</span>
+                                <span className={'HowMany'}>(43)</span>
+                            </Row>
+                            <Row onClick={()=>(alert("2 stars"))}>
+                                <i className="material-icons ratingStars">star</i>
+                                <i className="material-icons ratingStars">star</i>
+                                <i className="material-icons ratingStars">star_border</i>
+                                <i className="material-icons ratingStars">star_border</i>
+                                <i className="material-icons ratingStars">star_border</i>
+                                <span>& up</span>
+                                <span className={'HowMany'}>(43)</span>
+                            </Row>
+                            <Row onClick={()=>(alert("1 stars"))}>
+                                <i className="material-icons ratingStars">star</i>
+                                <i className="material-icons ratingStars">star_border</i>
+                                <i className="material-icons ratingStars">star_border</i>
+                                <i className="material-icons ratingStars">star_border</i>
+                                <i className="material-icons ratingStars">star_border</i>
+                                <span>& up</span>
+                                <span className={'HowMany'}>(43)</span>
+                            </Row>
+                        </Col>
+                        <hr/>
+                        <Button id={'filter-btn'}>CLEAR ALL FILTERS</Button>
                     </CardBody>
                 </Card>
             </React.Fragment>
