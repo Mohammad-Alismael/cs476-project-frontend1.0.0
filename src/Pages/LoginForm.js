67e73bfda043f7fd28e5cebab90768476e4aa810
mhd <mhdhd@gmail.com>
2021-04-02 20:03:33 +0400

    creating demo1 branch1.0

--- src/Pages/LoginForm.js
+++ src/Pages/LoginForm.js
@@ -1,19 +1,79 @@
-import React, {Component} from 'react';
-import {Card,CardBody} from 'reactstrap';
+import {Component,Link} from 'react';
+import '../Pages/css/LoginPage.css'
+import img from '../Pages/css/Design.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/LandingPage.css'
+import axios from 'axios';
+import GlobalContext from "../GlobalContext";
+import RegisterForm from "./RegisterForm";
 class LoginForm extends Component {
+    state = {
+        username: "",
+        password: ""
+    }
+    nextPath(path) {
+        this.props.history.push(path);
+    }
+    login = (e) =>{
+        e.preventDefault()
+        if (this.state.username != "" && this.state.password != ""){
+            axios.post('https://localhost:5001/api/account/login',{
+                "userName": this.state.username,
+                "password": this.state.password
+            }).then(res =>{
+                console.log(res.data)
+                this.context.updateEmail(res.data.email);
+                this.context.updateUseID(res.data.id);
+                this.context.updateUsername(res.data.userName)
+                if (res.data.userType == "Customer"){
+                    this.nextPath('/')
+                }else if (res.data.userType == "Sales Manager"){
+                    this.nextPath('/dashBoard')
+                }else if (res.data.userType == "Product owner"){
+                    this.nextPath('/product-owner-landing-page')
+                }else{
+                    this.nextPath('/')
+                }
+            }).catch(error =>{
+                if(error.response.status == 401)
+                    alert("username or password is incorrect")
+                console.log(error)
+            })
+        }else {
+            alert("check your info")
+        }
+        console.log(this.state)
+    }
+    updateSate = (e) =>{
+        e.preventDefault()
+        this.setState({[e.target.name] : e.target.value})
+    }
     render() {
         return (
-            <div>
-                <Card className="myCard">
-                    <CardBody>
+           <div class="container-fluid">
+            <div className="row">
+              <div className="col-lg-6 col-md-6 col-sm-6 left">
+                  <img className="middle-img" src={img} alt="" />
+              </div>
+          <div className="col-lg-4 col-md-4 col-sm-4 right">
+            <h1>Login</h1>
+                    <form className="user-info">
+                        <p className={"p-welcome"}>Welcome back, please login to your account.</p>
+                        <label className="email" for="email">username</label><br/>
+                        <input type="text" className="form-input" name="username" required onChange={this.updateSate}/><br/>
+                        <label className="password" for="password">Password</label><br/>
+                        <input type="password" className="form-input password-input" name="password" required onChange={this.updateSate}/><br/>
+                        <input type={"checkbox"} class="checkbox-form" name="terms" value="t"/>
+                        <label className="remember" htmlFor="terms"> Remember me</label>
+                        <a className="forgot-password" href="/forgot-password">Forgot Password?</a><br/>
+                        <button type="button" className="register" onClick={() => this.nextPath('/register')}>Register</button>
+                        <button type="button" className="login" onClick={this.login}>log In</button>
+              </form>
+          </div>
+        </div>
+  </div>
 
-                    </CardBody>
-                </Card>
-            </div>
         );
     }
 }
-
+LoginForm.contextType = GlobalContext
 export default LoginForm;
--- src/Pages/RegisterForm.js
+++ src/Pages/RegisterForm.js
@@ -1,13 +1,109 @@
 import React, {Component} from 'react';
-
+import '../Pages/css/LoginPage.css'
+import img from '../Pages/css/U.png'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Row, Container, Col, Input} from "reactstrap";
+import axios from 'axios';
+import GlobalContext from "../GlobalContext";
+import LandingPage from "./LandingPage";
 class RegisterForm extends Component {
+    state = {
+        username: "",
+        password: "",
+        email: "",
+        confirmPassword: "",
+        userType : ""
+    }
+    nextPath(path) {
+        this.props.history.push(path);
+    }
+    updateSate = (e) =>{
+        e.preventDefault()
+        this.setState({[e.target.name] : e.target.value})
+    }
+    createAccount = (e) =>{
+        e.preventDefault()
+        if (this.state.password == this.state.confirmPassword
+            && this.state.userType != ""){
+            axios.post(`https://localhost:5001/api/account/register`,{
+                "userName": this.state.username,
+                "name": "no data",
+                "surname": "no data",
+                "age": "no data",
+                "password": this.state.password,
+                "email": this.state.email,
+                "userType" : this.state.userType
+            })
+                .then(res => {
+                    const persons = res.data;
+                    // console.log(res)
+                    this.context.updateUsername(this.state.username)
+                    this.context.updateEmail(this.state.email)
+                    alert("successfully added")
+                    this.nextPath('/login')
+                }).catch(error =>{
+                    if(error.response.status == 400)
+                        alert("username is taken")
+                    alert("error happened")
+                    console.log(error)
+            })
+
+        }else{
+            if (this.state.userType == "" ||this.state.userType == "User Type"){
+                alert("choose user type")
+            }else{
+                alert("check your info")
+            }
+
+        }
+
+        // console.log(this.state)
+    }
     render() {
         return (
-            <div>
-                this is RegisterForm page
-            </div>
-        );
+            <Container fluid>
+                <Row>
+                    <div class="col-lg-6 col-md-6 col-sm-6 create-left" >
+                        <img className={"middle-img"} src={img} alt="" />
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4 create-right">
+                        <h1>Create Account</h1>
+                        <p>Fill the below form to create a new account.</p>
+                        <form class="user-info">
+                            <input type="text" class="form-input" name="username" placeholder="Username"
+                                   onChange={this.updateSate}
+                                   required/><br/>
+                            <label for="fname"></label><br/>
+                            <input type="email" className="form-input" name="email" id="email" placeholder="Email"
+                                   onChange={this.updateSate}
+                                   title={"@gmail.com"}
+                                   required pattern={'/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$/'}/><br/>
+                            <label for="password"></label><br/>
+                            <input type="password" class="form-input" name="password" placeholder="Password"
+                                   onChange={this.updateSate}
+                                   required/><br/>
+                            <label for="confirm-password"></label><br/>
+                            <input type="password" className="form-input confirm-password" name="confirmPassword" placeholder="Confirm Password"
+                                   onChange={this.updateSate}required/><br/>
+                            <Input type="select" name="userType" className="userType"
+                                   onChange={this.updateSate}
+                                   style={{border : '1px solid rgb(111, 107, 232)',height : '3rem',marginBottom: '8px'}}>
+                                <option>User Type</option>
+                                <option>Sales Manager</option>
+                                <option>Customer</option>
+                                <option>Product owner</option>
+                            </Input>
+                            <input type="checkbox" class="checkbox-form" name="terms" value="t"/>
+                            <label for="terms"> I accept the terms & conditions.</label><br/>
+                            <button className="register" type="button" name="button"
+                                    onClick={this.createAccount}
+                                    style={{marginTop: '10px'}}>Log in</button>
+                        </form>
+                    </div>
+                </Row>
+            </Container>
+    );
     }
 }
-
+RegisterForm.contextType = GlobalContext
 export default RegisterForm;
