2ac3385a51e03d8a9d2aa76c6f30bfcd4ff483bc
mhd <mhdhd@gmail.com>
2021-04-15 22:35:05 +0400

    adding "add, edit and delete"product pages

---
+++ src/Pages/DeleteProduct.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class DeleteProduct extends Component {
+    render() {
+        return (
+            <div>
+                this is delete product page
+            </div>
+        );
+    }
+}
+
+export default DeleteProduct;
---
+++ src/Pages/EditProduct.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class EditProduct extends Component {
+    render() {
+        return (
+            <div>
+                this is edit product page
+            </div>
+        );
+    }
+}
+
+export default EditProduct;
