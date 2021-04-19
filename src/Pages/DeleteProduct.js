ccc46975d6747f2ac0b72033d81c8ea1e2026e22
mhd <mhdhd@gmail.com>
2021-04-19 14:02:37 +0400

    working on deleting page

--- src/Pages/DeleteProduct.js
+++ src/Pages/DeleteProduct.js
@@ -1,11 +1,43 @@
 import React, {Component} from 'react';
+import {Card, Container} from "reactstrap";
+import ShoppingCartItems from "../Components/ShoppingCartItems";
+import tmp2 from "../Images/2.png";
 
 class DeleteProduct extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            childern : [true,true]
+        };
+        this.handleChildUnmount = this.handleChildUnmount.bind(this);
+    }
+    handleChildUnmount(){
+        this.setState({renderChild: false});
+    }
     render() {
         return (
-            <div>
-                this is delete product page
-            </div>
+            <Container>
+                {this.state.renderChild ?
+                <ShoppingCartItems
+                    productName={"msi"}
+                    price={2000}
+                    brand={"microsoft"}
+                    rating={1}
+                    quantity={10}
+                    srcImg={tmp2}
+                    unmountMe={this.handleChildUnmount}
+                /> : null }
+                {this.state.renderChild ?
+                    <ShoppingCartItems
+                        productName={"msi"}
+                        price={2000}
+                        brand={"microsoft"}
+                        rating={1}
+                        quantity={10}
+                        srcImg={tmp2}
+                        unmountMe={this.handleChildUnmount}
+                    /> : null }
+            </Container>
         );
     }
 }
