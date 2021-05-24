e1da116690f1242ba7633bf7115a424225f5cd27
mhd <mhdhd@gmail.com>
2021-05-24 14:53:07 +0400

    funding page

--- src/Pages/FundPage.js
+++ src/Pages/FundPage.js
@@ -3,20 +3,91 @@ import React, {Component} from 'react';
 class FundPage extends Component {
     render() {
         return (
-            <div>
-                <h1>Lets give you some funds</h1>
-                <h2>Choose an amount to add to your wallet:</h2>
-                <select id="cars" name="cars">
-                    <option value="10">10TL</option>
-                    <option value="20">20TL</option>
-                    <option value="50">50TL</option>
-                    <option value="100">100TL</option>
-                    <option value="200">200TL</option>
-                    <option value="500">500TL</option>
-                    <option value="1000">1000TL</option>
-                </select><br/>
-                <input type="submit" name="submit" value="Submit"/>
-                    <h2>Click the "Submit" button to receive the selected TL to your wallet.</h2>
+            <div className={'fundPageDiv'}>
+                <section className="white-section" id="pricing">
+                    {/*<h2 className="section-heading">Please choose how much money you would like to upload</h2>*/}
+                    <div className="row">
+                        <div className="pricing-column col-lg-4 col-md-6">
+                            <div className="card">
+                                <div className="card-header">
+                                    <h3>10₺</h3>
+                                </div>
+                                <div className="card-body">
+                                    <h2 className="price-text">10 TL will be transferred to your account</h2>
+                                    <p>You cannot earn extra shopping points</p>
+                                    <button className="btn btn-lg btn-block btn-dark" type="button">Submit</button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="pricing-column col-lg-4 col-md-6">
+                            <div className="card">
+                                <div className="card-header">
+                                    <h3>20₺</h3>
+                                </div>
+                                <div className="card-body">
+                                    <h2 className="price-text">20 TL will be transferred to your account</h2>
+                                    <p>You can earn 0.2TL extra shopping points</p>
+
+                                    <button className="btn btn-lg btn-block btn-dark" type="button">Submit</button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="pricing-column col-lg-4">
+                            <div className="card">
+                                <div className="card-header">
+                                    <h3>50₺</h3>
+                                </div>
+                                <div className="card-body">
+                                    <h2 className="price-text">50 TL will be transferred to your account</h2>
+                                    <p>You can earn 0.6TL extra shopping points</p>
+                                    <button className="btn btn-lg btn-block btn-dark" type="button">Submit</button>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="pricing-column col-lg-4">
+                            <div className="card">
+                                <div className="card-header">
+                                    <h3>100₺</h3>
+                                </div>
+                                <div className="card-body">
+                                    <h2 className="price-text">100 TL will be transferred to your account</h2>
+                                    <p>You can earn 1.3TL extra shopping points</p>
+                                    <button className="btn btn-lg btn-block btn-dark" type="button">Submit</button>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="pricing-column col-lg-4">
+                            <div className="card">
+                                <div className="card-header">
+                                    <h3>200₺</h3>
+                                </div>
+                                <div className="card-body">
+                                    <h2 className="price-text">200 TL will be transferred to your account</h2>
+                                    <p>You can earn 2.7TL extra shopping points</p>
+                                    <button className="btn btn-lg btn-block btn-dark" type="button">Submit</button>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="pricing-column col-lg-4">
+                            <div className="card">
+                                <div className="card-header">
+                                    <h3>500₺</h3>
+                                </div>
+                                <div className="card-body">
+                                    <h2 className="price-text">500 TL will be transferred to your account</h2>
+                                    <p>You can earn 5.5TL extra shopping points</p>
+                                    <button className="btn btn-lg btn-block btn-dark" type="button">Submit</button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         );
     }
