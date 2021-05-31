d3ece452de61b618fd6ccaca93c01db3c0072213
mhd <mhdhd@gmail.com>
2021-05-31 22:55:59 +0400

    adding loading screen for login

--- src/Pages/LoginForm.js
+++ src/Pages/LoginForm.js
@@ -5,18 +5,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import GlobalContext from "../GlobalContext";
 import {toast} from "react-toastify";
-import {Button, Card, Col, Container, Row} from "reactstrap";
+import {Button, Card, Col, Container, Row, Spinner} from "reactstrap";
 
 class LoginForm extends Component {
     state = {
         username: "",
-        password: ""
+        password: "",
+        loading: false
     }
     nextPath(path) {
         this.props.history.push(path);
     }
     login = (e) =>{
         e.preventDefault()
+        this.setState({loading: true})
         if (this.state.username != "" && this.state.password != ""){
             axios.post('https://localhost:5001/api/account/login',{
                 "userName": this.state.username,
@@ -45,6 +47,7 @@ class LoginForm extends Component {
                 //     console.log(this.context.userID)
                 // }
             }).catch(error =>{
+                this.setState({loading: false})
                 console.log(error)
                     toast.error("username or password is incorrect")
 
@@ -58,28 +61,38 @@ class LoginForm extends Component {
         this.setState({[e.target.name] : e.target.value})
     }
     render() {
-        return (
-           <Container fuild className={'noPadding'}>
-                <Row>
-                  <Col lg={6} md={6} sm={0} className="left">
-                      <img className="middle-img" src={img} alt="" />
-                  </Col>
-                    <Col lg={4} md={4} sm={12} xs={12} className="right" >
-                        <h1>Login</h1>
-                        <div className="user-info">
-                            <p>Welcome back, please login to your account.</p>
-                            <label className="email" for="email">username</label><br/>
-                            <input type="text" className="form-input" name="username" required onChange={this.updateSate}/><br/>
-                            <label className="password" for="password">Password</label><br/>
-                            <input type="password" className="form-input password-input" name="password" required onChange={this.updateSate}/><br/>
-                            <button type="button" className="register" onClick={() => this.nextPath('/register')}>Register</button>
-                            <button type="button" className="login" onClick={this.login}>log In</button>
-                        </div>
-                    </Col>
-                </Row>
-           </Container>
+        if (!this.state.loading){
+            return (
+                <Container fuild className={'noPadding'}>
+                    <Row>
+                        <Col lg={6} md={6} sm={0} className="left">
+                            <img className="middle-img" src={img} alt="" />
+                        </Col>
+                        <Col lg={4} md={4} sm={12} xs={12} className="right" >
+                            <h1>Login</h1>
+                            <div className="user-info">
+                                <p>Welcome back, please login to your account.</p>
+                                <label className="email" for="email">username</label><br/>
+                                <input type="text" className="form-input" name="username" required onChange={this.updateSate}/><br/>
+                                <label className="password" for="password">Password</label><br/>
+                                <input type="password" className="form-input password-input" name="password" required onChange={this.updateSate}/><br/>
+                                <button type="button" className="register" onClick={() => this.nextPath('/register')}>Register</button>
+                                <button type="button" className="login" onClick={this.login}>log In</button>
+                            </div>
+                        </Col>
+                    </Row>
 
-        );
+                </Container>
+
+            );
+        }else {
+            return (
+                <div>
+                    <Spinner color="black" style={{ width: '20rem', height: '20rem',marginLeft: '45%',marginTop: '10%',marginBottom: '10%' }}/>
+                </div>
+            )
+        }
+
     }
 }
 LoginForm.contextType = GlobalContext
